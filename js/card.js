475eb1ef2c613a28edf6261db5901ddfeb404af1
Shelby Clayton <shelby_clayton@hotmail.com>
2017-05-25 19:17:00 -0700

    shuffle method

---
+++ js/card.js
@@ -0,0 +1,6 @@
+function Card(value, suit, rank, image) {
+  this.value = value;
+  this.suit = suit;
+  this.rank = rank;
+  this.image = image;
+}
---
+++ js/shuffle.js
@@ -0,0 +1,88 @@
+function shuffle(array) {
+  var generatedIndex = 0;
+  var outcome = [];
+  for (var i = array.length - 1; i >= 0; i--) {
+    generatedIndex = Math.floor(Math.random() * (i + 1));
+    outcome.push(array[generatedIndex]);
+    array.splice(generatedIndex, 1);
+  }
+  return outcome;
+}
+
+$(document).ready(function() {
+  $(".shuffle-button").click(function() {
+    var deck = [
+      '<img src="images/ace_of_hearts.png">',
+      '<img src="images/king_of_hearts.png">',
+      '<img src="images/queen_of_hearts.png">',
+      '<img src="images/jack_of_hearts.png">',
+      '<img src="images/10_of_hearts.png">',
+      '<img src="images/9_of_hearts.png">',
+      '<img src="images/8_of_hearts.png">',
+      '<img src="images/7_of_hearts.png">',
+      '<img src="images/6_of_hearts.png">',
+      '<img src="images/5_of_hearts.png">',
+      '<img src="images/4_of_hearts.png">',
+      '<img src="images/3_of_hearts.png">',
+      '<img src="images/2_of_hearts.png">',
+      '<img src="images/ace_of_spades.png">',
+      '<img src="images/king_of_spades.png">',
+      '<img src="images/queen_of_spades.png">',
+      '<img src="images/jack_of_spades.png">',
+      '<img src="images/10_of_spades.png">',
+      '<img src="images/9_of_spades.png">',
+      '<img src="images/8_of_spades.png">',
+      '<img src="images/7_of_spades.png">',
+      '<img src="images/6_of_spades.png">',
+      '<img src="images/5_of_spades.png">',
+      '<img src="images/4_of_spades.png">',
+      '<img src="images/3_of_spades.png">',
+      '<img src="images/2_of_spades.png">',
+      '<img src="images/ace_of_diamonds.png">',
+      '<img src="images/king_of_diamonds.png">',
+      '<img src="images/queen_of_diamonds.png">',
+      '<img src="images/jack_of_diamonds.png">',
+      '<img src="images/10_of_diamonds.png">',
+      '<img src="images/9_of_diamonds.png">',
+      '<img src="images/8_of_diamonds.png">',
+      '<img src="images/7_of_diamonds.png">',
+      '<img src="images/6_of_diamonds.png">',
+      '<img src="images/5_of_diamonds.png">',
+      '<img src="images/4_of_diamonds.png">',
+      '<img src="images/3_of_diamonds.png">',
+      '<img src="images/2_of_diamonds.png">',
+      '<img src="images/ace_of_clubs.png">',
+      '<img src="images/king_of_clubs.png">',
+      '<img src="images/queen_of_clubs.png">',
+      '<img src="images/jack_of_clubs.png">',
+      '<img src="images/10_of_clubs.png">',
+      '<img src="images/9_of_clubs.png">',
+      '<img src="images/8_of_clubs.png">',
+      '<img src="images/7_of_clubs.png">',
+      '<img src="images/6_of_clubs.png">',
+      '<img src="images/5_of_clubs.png">',
+      '<img src="images/4_of_clubs.png">',
+      '<img src="images/3_of_clubs.png">',
+      '<img src="images/2_of_clubs.png">'];
+    $("#results").empty();
+    $(".card-backs").hide();
+    $(".ordered-cards").hide();
+    $("#results").show();
+    var randomDeck = shuffle(deck);
+    $('#results').append(randomDeck);
+  });
+  // $(".sort-button").click(function() {
+  //   $(".card-backs").hide();
+  //   $("#results").hide();
+  //   $(".ordered-cards").show();
+  // });
+
+
+
+
+  $(".sort-button").click(function() {
+    $(".card-backs").hide();
+    $("#results").hide();
+    $(".ordered-cards").show();
+  });
+});
